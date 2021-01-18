4211e79d6ca5218a79db54565acf67ba905e7d8a
kant9612 <kanstephentakayama@gmail.com>
2021-01-18 07:15:55 +0000

    [add] 4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_path
+  end
+
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params_require(:user).permit(:name, :introduction, :profile_image_id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   # マイページを作成する
-  resources :users, only: [:show]
+  resources :users
+  resources :books
 end
