4b08ccd6e8d02d21c96a0e100f6f793c91c61471
kant9612 <kanstephentakayama@gmail.com>
2021-01-19 04:08:14 +0000

    [add] 4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,9 +33,9 @@ class UsersController < ApplicationController
 
    private
 
-  def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
-  end
+   def user_params
+     params.require(:user).permit(:name, :introduction, :profile_image)
+   end
 
 
 end
