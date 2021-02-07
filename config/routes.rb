4165206e4593b1db3b092a6c9ce023e7e678a5f7
kant9612 <kanstephentakayama@gmail.com>
2021-02-07 15:42:38 +0000

    いいね機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#top'
+  root :to => "homes#top"
   get "/home/about" => 'homes#about'
 
   resources :users, only: [:show, :edit, :update, :index]
-  resources :books
+
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
