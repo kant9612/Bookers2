98fa4a41428933237cd474b42109a9a5b1770b8f
kant9612 <kanstephentakayama@gmail.com>
2021-02-08 13:29:47 +0000

    非同期通信（コメント機能）未完成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -7,7 +7,7 @@ class BookCommentsController < ApplicationController
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
 		if @book_comment.save
-  		redirect_to book_path(@book.id)
+  		  render 'index'
 		else
 		  render 'books/show'
 		end
@@ -15,9 +15,9 @@ class BookCommentsController < ApplicationController
 
 	def destroy
 		@book = Book.find(params[:book_id])
-  	book_comment = @book.book_comments.find(params[:id])
+  		book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
-		redirect_to request.referer
+		render 'index'
 	end
 
 	private
