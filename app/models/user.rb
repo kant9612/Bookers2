232c6c9f4ec33af3b4a609cfe0ae8712b6c27baa
kant9612 <kanstephentakayama@gmail.com>
2021-02-10 12:25:12 +0000

    [add]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,23 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   attachment :profile_image
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
   # ユーザー名前と紹介文に対するバリデーション
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
